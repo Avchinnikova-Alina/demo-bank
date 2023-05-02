01021b8e310501e3c725551fd7406da119bba608
Avchinnikova Alina <avchinnikova1012@gmail.com>
2023-05-02 22:54:07 +0300

    add useaccount class

---
+++ UseAccount.java
@@ -0,0 +1,15 @@
+class UseAccount {
+    public static void main(String[] args) {
+        Account myAccount = new Account();
+        Account yourAccount = new Account();
+        myAccount.name = "Барри";
+        myAccount.address = "222 Inner Lane";
+        myAccount.balance = 24.02;
+        yourAccount.name = "Джейн";
+        yourAccount.address = "121 Outer Street";
+        yourAccount.balance= 55.63;
+        myAccount.display();
+        System.out.println();
+        yourAccount.display();
+    }
+}
